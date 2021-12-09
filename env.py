16b5d56e8b9d96c177a495af4d4482616789044c
Iryna <48490619+irasan@users.noreply.github.com>
2021-12-09 17:41:31 +0000

    Delete env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "Av6EIOqI>&KtCWkAL-QmBpC0IH,#4$")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:Veselka123@myFirstCluster.xt8sr.mongodb.net/elf_service?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "elf_service")

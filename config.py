85837c94c673d1304befb2c94c9d7e92eb0bf410
irasan <iryna.sanzhara@gmail.com>
2021-12-10 18:17:31 +0000

    Commit in order to restart the workspace

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://something-here'
+    MONGO_DBNAME = os.environ.get('MONGO_DBNAME') or 'elf_service'

36a7288c0d9ae21965265b28312d9a1909f1ed13
irasan <iryna.sanzhara@gmail.com>
2021-12-13 11:59:20 +0000

    Fix typo in letter to santa - matching say hi's

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def download_letter(child_id):
         gift2 = f"I also wish for a {child.get('friend')} for my friend."
         last_sentence = "I love snacking on chocolate pretzels these days, and I'll be leaving a plate of that by the tree. "\
             "I hope that will give you energy as you drop off gifts to the other nice kids out there."
-        ps = f"P.S. Say 'hi' to {child.get('say_hi1')} and {child.get('say_hi1')} for me!"
+        ps = f"P.S. Say 'hi' to {child.get('say_hi1')} and {child.get('say_hi2')} for me!"
         closing = "Merry Christmas, Santa!"
     
     # save FPDF() class into a
